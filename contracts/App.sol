850c5f0e6fe0726cd6a66de156d9ff2c80facd9e
Oliver <onbjerg@users.noreply.github.com>
2017-11-15 16:19:02 +0100

    Fix indentation

--- contracts/App.sol
+++ contracts/App.sol
@@ -3,28 +3,25 @@ pragma solidity ^0.4.4;
 import "@aragon/core/contracts/apps/App.sol";
 import "@aragon/core/contracts/common/Initializable.sol";
 
-////
-////	           ....
-////	       .,,,,..,,,,.
-////	   ..,,.. ..     .,,,..
-////	 .,,.  ..,:....,,..  .,,.
-////	,:   ...,.    .,,..,.   :,
-////	.:. ,. ,           ,.. .:.
-////	 ,:,.  ..        .,,., :,
-////	  ,;.   ........,..,..:,
-////	   ,:.       .. .....:,
-////	    .:,           .::.
-////	      .,,.      .,,.
-////	        .,,,..,,,.
-////	           ....
-////
-////  Build something beautiful.
-
 contract Application is Initializable, App {
-	  function initialize(string _name) onlyInit
-		{
-		    initialized();
-
-				///
-	  }
+	////
+	////	           ....
+	////	       .,,,,..,,,,.
+	////	   ..,,.. ..     .,,,..
+	////	 .,,.  ..,:....,,..  .,,.
+	////	,:   ...,.    .,,..,.   :,
+	////	.:. ,. ,           ,.. .:.
+	////	 ,:,.  ..        .,,., :,
+	////	  ,;.   ........,..,..:,
+	////	   ,:.       .. .....:,
+	////	    .:,           .::.
+	////	      .,,.      .,,.
+	////	        .,,,..,,,.
+	////	           ....
+	////
+	////  Build something beautiful.
+	function initialize(string _name) onlyInit
+	{
+		initialized();
+	}
 }
