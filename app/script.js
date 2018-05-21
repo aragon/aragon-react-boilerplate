6779f10f99cf5f10f3f5207d027cd1bc23e76e6c
Brett Sun <qisheng.brett.sun@gmail.com>
2018-05-21 16:34:19 -0600

    Use parseInt(<num>, 10)

--- app/script.js
+++ app/script.js
@@ -10,9 +10,9 @@ app.store((state, event) => {
 
   switch (event.event) {
     case 'Increment':
-      return { count: parseInt(state.count) + parseInt(event.returnValues.step) }
+      return { count: parseInt(state.count, 10) + parseInt(event.returnValues.step, 10) }
     case 'Decrement':
-      return { count: parseInt(state.count) - parseInt(event.returnValues.step) }
+      return { count: parseInt(state.count, 10) - parseInt(event.returnValues.step, 10) }
     default:
       return state
   }
