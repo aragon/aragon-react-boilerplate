24432ccccdd04aa6cf19e3a56c47a0f050e14cc6
ßingen <bingentxu@gmail.com>
2018-05-23 21:25:49 +0200

    Adapt frontend app to the same style as other aragon apps
    Use babel.
    USe SafeMath for contract.

--- contracts/CounterApp.sol
+++ contracts/CounterApp.sol
@@ -1,26 +1,29 @@
 pragma solidity ^0.4.4;
 
 import "@aragon/os/contracts/apps/AragonApp.sol";
+import "@aragon/os/lib/zeppelin/math/SafeMath.sol";
 
 contract CounterApp is AragonApp {
+    using SafeMath for uint256;
+
     /// Events
-    event Increment(address indexed entity, int step);
-    event Decrement(address indexed entity, int step);
+    event Increment(address indexed entity, uint256 step);
+    event Decrement(address indexed entity, uint256 step);
 
     /// State
-    int public value;
+    uint256 public value;
 
     /// ACL
     bytes32 constant public INCREMENT_ROLE = keccak256("INCREMENT_ROLE");
     bytes32 constant public DECREMENT_ROLE = keccak256("DECREMENT_ROLE");
-    
-    function increment(int step) auth(INCREMENT_ROLE) external {
-        value += step;
+
+    function increment(uint256 step) auth(INCREMENT_ROLE) external {
+        value = value.add(step);
         Increment(msg.sender, step);
     }
 
-    function decrement(int step) auth(DECREMENT_ROLE) external {
-        value -= step;
+    function decrement(uint256 step) auth(DECREMENT_ROLE) external {
+        value = value.sub(step);
         Decrement(msg.sender, step);
     }
 }
