169a3b46937389d23a0a7d314ac10869872e4fb9
Pierre Bertet <hello@pierre.world>
2019-03-12 16:36:17 +0000

    Use @aragon/api-react
    Also updates the structure to separate the webapp as an independent
    module.

---
+++ app/src/App.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { useAragonApi } from '@aragon/api-react'
+import { Main, Button } from '@aragon/ui'
+import styled from 'styled-components'
+
+const appStateReducer = state => {
+  if (state === null) {
+    return { count: 0, syncing: true }
+  }
+  return state
+}
+
+function App() {
+  const { api, appState } = useAragonApi(appStateReducer)
+  const { count, syncing } = appState
+  return (
+    <Main>
+      <BaseLayout>
+        {syncing && <Syncing />}
+        <Count>Count: {count}</Count>
+        <Buttons>
+          <Button mode="secondary" onClick={() => api.decrement(1)}>
+            Decrement
+          </Button>
+          <Button mode="secondary" onClick={() => api.increment(1)}>
+            Increment
+          </Button>
+        </Buttons>
+      </BaseLayout>
+    </Main>
+  )
+}
+
+const BaseLayout = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  flex-direction: column;
+`
+
+const Count = styled.h1`
+  font-size: 30px;
+`
+
+const Buttons = styled.div`
+  display: grid;
+  grid-auto-flow: column;
+  grid-gap: 40px;
+  margin-top: 20px;
+`
+
+const Syncing = styled.div.attrs({ children: 'Syncing…' })`
+  position: absolute;
+  top: 15px;
+  right: 20px;
+`
+
+export default App
