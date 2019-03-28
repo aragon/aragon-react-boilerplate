2b9e4d95c48c94f5f36f413322f4eb64514a32fb
David Llop <931684+sembrestels@users.noreply.github.com>
2019-03-28 10:39:12 +0100

    Pull over aragon-apps' .solcover.js

--- .solcover.js
+++ .solcover.js
@@ -1,9 +1,7 @@
 module.exports = {
     norpc: true,
-    compileCommand: '../node_modules/.bin/truffle compile',
-    testCommand: '../node_modules/.bin/truffle test --network coverage',
     copyPackages: [],
     skipFiles: [
-        'test/TestImports.sol',
+        'test',
     ]
 }
