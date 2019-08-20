4dca801c6cbdc57b29d9099c847a89de9730b50d
Alejandro J. Santander <palebluedot@gmail.com>
2019-08-20 12:31:23 -0600

    Simplified tests a bit

--- test/app.js
+++ test/app.js
@@ -6,26 +6,21 @@ const CounterApp = artifacts.require('CounterApp.sol')
 const ANY_ADDRESS = '0xffffffffffffffffffffffffffffffffffffffff'
 
 contract('CounterApp', ([_, appManager, user]) => {
-  let dao, acl, app
+  let app
 
-  beforeEach('deploy dao', async () => {
-    ({dao, acl} = await deployDAO(appManager))
-  })
+  beforeEach('deploy dao and app', async () => {
+    const { dao, acl } = await deployDAO(appManager)
 
-  beforeEach('deploy and initialize app', async () => {
-
-    // Deploy the app's logic implementation or "base" contract.
+    // Deploy the app's "base" contract.
     const appBase = await CounterApp.new()
 
-    // Instantiate a proxy for the app.
+    // Instantiate a proxy for the app, using the base contract as its logic implementation.
     const instanceReceipt = await dao.newAppInstance('0x1234', appBase.address, '0x', false, { from: appManager })
     app = CounterApp.at(getEventFromReceipt(instanceReceipt, 'NewAppProxy').args.proxy)
 
-    // Grant app permissions.
-    const INCREMENT_ROLE = await app.INCREMENT_ROLE()
-    const DECREMENT_ROLE = await app.DECREMENT_ROLE()
-    await acl.createPermission(ANY_ADDRESS, app.address, INCREMENT_ROLE, appManager, { from: appManager, })
-    await acl.createPermission(ANY_ADDRESS, app.address, DECREMENT_ROLE, appManager, { from: appManager, })
+    // Set up the app's permissions.
+    await acl.createPermission(ANY_ADDRESS, app.address, await app.INCREMENT_ROLE(), appManager, { from: appManager, })
+    await acl.createPermission(ANY_ADDRESS, app.address, await app.DECREMENT_ROLE(), appManager, { from: appManager, })
 
     await app.initialize()
   })
