e992539211a3d9c59e076c2ffd93f2db433e52e5
Mathew Cormier <mathew.corm@gmail.com>
2020-01-09 15:34:53 -0500

    Move buttons close to counter and add array check

--- app/src/App.js
+++ app/src/App.js
@@ -10,6 +10,7 @@ import {
   Main,
   SyncIndicator,
   Tabs,
+  Text,
   textStyle,
 } from '@aragon/ui'
 import styled from 'styled-components'
@@ -19,7 +20,9 @@ function App() {
   const { count, isSyncing } = appState
 
   const pathParts = path.match(/^\/tab\/([0-9]+)/)
-  const pageIndex = pathParts ? parseInt(pathParts[1], 10) - 1 : 0
+  const pageIndex = Array.isArray(pathParts)
+    ? parseInt(pathParts[1], 10) - 1
+    : 0
 
   return (
     <Main>
@@ -27,23 +30,13 @@ function App() {
       <Header
         primary="Counter"
         secondary={
-          <>
-            <Button
-              display="icon"
-              icon={<IconMinus />}
-              label="Decrement"
-              onClick={() => api.decrement(1).toPromise()}
-            />
-            <Button
-              display="icon"
-              icon={<IconPlus />}
-              label="Increment"
-              onClick={() => api.increment(1).toPromise()}
-              css={`
-                margin-left: ${2 * GU}px;
-              `}
-            />
-          </>
+          <Text
+            css={`
+              ${textStyle('title2')}
+            `}
+          >
+            {count}
+          </Text>
         }
       />
       <Tabs
@@ -56,14 +49,39 @@ function App() {
           display: flex;
           align-items: center;
           justify-content: center;
+          text-align: center;
           height: ${50 * GU}px;
           ${textStyle('title3')};
         `}
       >
         Count: {count}
+        <Buttons>
+          <Button
+            display="icon"
+            icon={<IconMinus />}
+            label="Decrement"
+            onClick={() => api.decrement(1).toPromise()}
+          />
+          <Button
+            display="icon"
+            icon={<IconPlus />}
+            label="Increment"
+            onClick={() => api.increment(1).toPromise()}
+            css={`
+              margin-left: ${2 * GU}px;
+            `}
+          />
+        </Buttons>
       </Box>
     </Main>
   )
 }
+
+const Buttons = styled.div`
+  display: grid;
+  grid-auto-flow: column;
+  grid-gap: 40px;
+  margin-top: 20px;
+`
 
 export default App
